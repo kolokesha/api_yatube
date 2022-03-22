c97d83493ac059283edbb6f3af5d244208fcd6eb
kolokesha <kolokesha@rambler.ru>
2022-03-23 01:17:29 +0300

    fixed urls + permissions

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,12 +6,12 @@ from .views import CommentViewSet, GroupViewSet, PostViewSet
 
 router = DefaultRouter()
 
-router.register('v1/groups', GroupViewSet)
-router.register('v1/posts', PostViewSet)
-router.register(r'v1/posts/(?P<post_id>\d+)/comments', CommentViewSet,
+router.register('groups', GroupViewSet, basename='groups')
+router.register('posts', PostViewSet, basename='posts')
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
                 basename='comments')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(router.urls)),
     path('v1/api-token-auth/', views.obtain_auth_token),
 ]
