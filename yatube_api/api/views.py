f3fdb08b22658b296fe5e2f7bb9c8b2d7584f8e8
kolokesha <kolokesha@rambler.ru>
2022-03-22 06:31:25 +0300

    added minor fixes

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
-from posts.models import Comment, Group, Post
+from posts.models import Group, Post
 from rest_framework import viewsets
 
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
